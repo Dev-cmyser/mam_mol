efdd7a88aee64cdd769a13bbaede11a7f9114312
Dev-cmyser <Dev-cmyser@users.noreply.github.com>
2026-03-15 07:56:13 +0000

    Deploying to gh-pages from @ Dev-cmyser/mam_mol@9d403b16a223d61e455a9934682effa518755d08 🚀

--- app/docs/-/node.d.ts
+++ app/docs/-/node.d.ts
@@ -5753,6 +5753,7 @@ declare namespace $ {
 		width_px( ): string
 		sub( ): readonly(any)[]
 		resizing( next?: boolean ): boolean
+		transforming( ): boolean
 		height_min( ): number
 		width_min( ): number
 		height( next?: number ): number
